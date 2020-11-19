e5e302662df0b9918b44085b3cfe7a3dfb40df67
zhang.xiaokang <1771977110@qq.com>
2020-11-19 18:59:53 +0800

    ci(config.js): update commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  extend: ['@commitlint/config-conventional'],
+  extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', [
       'build', 'ci', 'feat', 'fix', 'perf', 'refactor', 'docs', 'style', 'revert']],
     'type-case': [0],
     'type-empty': [0],
-    'scope-empty': [0],
+    'scope-empty': [2, 'never'],
     'scope-case': [0],
     'subject-full-stop': [0, 'never'],
     'subject-case': [0, 'never'],
